a3f6af655bcea0e244dc3896f00a1fd186178068
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-08-17 18:25:28 +0530

    Supplier api done.

---
+++ src/routes/supplier.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import SupplierController from "../controllers/supplier.controller";
+
+const router = Router();
+
+router.post('/suppliers', SupplierController.createSupplier);
+router.get('/suppliers', SupplierController.getAllSuppliers);
+router.get('/suppliers/:id', SupplierController.getSupplierById);
+router.put('/suppliers/:id', SupplierController.updateSupplier);
+router.delete('/suppliers/:id', SupplierController.deleteSupplier);
+
+
+export default router;
---
+++ src/services/supplier.service.ts
@@ -0,0 +1,28 @@
+import Supplier, {ISupplier} from "../models/supplier";
+
+
+class SupplierService {
+    async createSupplier(data: Partial<ISupplier>): Promise<ISupplier> {
+        const supplier = new Supplier(data);
+        await supplier.save();
+        return supplier;
+    }
+
+    async getAllSuppliers(): Promise<ISupplier[]> {
+        return Supplier.find().exec();
+    }
+
+    async getSupplierById(id: string): Promise<ISupplier | null> {
+        return Supplier.findById(id).exec();
+    }
+
+    async updateSupplier(id: string, data: Partial<ISupplier>): Promise<ISupplier | null> {
+        return Supplier.findByIdAndUpdate(id, data, { new: true }).exec();
+    }
+
+    async deleteSupplier(id: string): Promise<ISupplier | null> {
+        return Supplier.findByIdAndDelete(id).exec();
+    }
+}
+
+export default new SupplierService();
